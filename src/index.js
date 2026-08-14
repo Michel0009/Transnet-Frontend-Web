2f6f35f905464f1dad88096725e93ac25b130e76
Michel0009 <michaellutfallah007@gmail.com>
2026-08-14 03:11:43 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
